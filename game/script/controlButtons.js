f63bfb6c861474f00daa15f37661e70a5e40a630
AsQqqq <asqdanil@yandex.ru>
2025-04-24 01:56:42 +0300

    Добавлены кнопки пуск и стоп, пока в режиме разработки

---
+++ game/script/controlButtons.js
@@ -0,0 +1,10 @@
+const playButton = document.getElementById('playButton');
+const stopButton = document.getElementById('stopButton');
+
+playButton.addEventListener('click', () => {
+    alert('Запуск пока еще не сделан, в разработке');
+});
+
+stopButton.addEventListener('click', () => {
+    alert('Остановить пока невозможно, в разработке');
+});
